9f082d876702fece35a6e0fbb1707dc85ba71075
PinTrees <59812031+PinTrees@users.noreply.github.com>
2024-09-23 16:16:21 +0900

    add launcher file

--- Assets/_Script/System/Graphic-System/GraphicManager.cs
+++ Assets/_Script/System/Graphic-System/GraphicManager.cs
@@ -1,6 +1,6 @@
-using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering;
+using UnityEngine;
 
 public class GraphicManager : Singleton<GraphicManager>
 {
@@ -8,42 +8,11 @@ public class GraphicManager : Singleton<GraphicManager>
     [Range(0.5f, 1)]
     public float renderScale = 0.5f;
 
-
     public override void Init()
     {
         base.Init();
 
         Application.targetFrameRate = targetFrameRate;
         QualitySettings.vSyncCount = 0;
-
-        SetDynamicResolution(true);
-        SetRenderScale(renderScale); 
-    }
-
-    private void SetDynamicResolution(bool enable)
-    {
-        HDRenderPipelineAsset hdrpAsset = (HDRenderPipelineAsset)GraphicsSettings.defaultRenderPipeline;
-        if (hdrpAsset != null)
-        {
-            var dynamicResolutionSettings = hdrpAsset.currentPlatformRenderPipelineSettings.dynamicResolutionSettings;
-            dynamicResolutionSettings.enabled = enable;
-            Debug.Log($"Dynamic Resolution enabled: {enable}");
-        }
-        else
-        {
-            Debug.LogError("HDRP Asset not found!");
-        }
-    }
-
-    private void SetRenderScale(float scale)
-    {
-        if (scale < 0.5f || scale > 1.0f)
-        {
-            Debug.LogError("Render scale should be between 0.5 and 1.0");
-            return;
-        }
-
-        ScalableBufferManager.ResizeBuffers(scale, scale);
-        Debug.Log($"Render Scale set to {scale}");
     }
 }
